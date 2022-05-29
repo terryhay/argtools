6680ece38c57100ee7dfd8c1d8156b7b30fe13fc
terryhay <terryhay@yandex.ru>
2022-05-29 13:45:17 +0300

    test cases for ArgValue to float

---
+++ pkg/parsedData/test/test_arg_value/to_float64_test.go
@@ -0,0 +1,72 @@
+package test_arg_value
+
+import (
+	"fmt"
+	"github.com/brianvoe/gofakeit"
+	"github.com/stretchr/testify/require"
+	"github.com/terryhay/argtools/pkg/parsedData"
+	"testing"
+)
+
+func TestArgValueToFloat64(t *testing.T) {
+	t.Parallel()
+
+	randNotNullValue := gofakeit.Float64()
+	for randNotNullValue == 0.0 {
+		randNotNullValue = gofakeit.Float64()
+	}
+
+	randPositiveValue := randNotNullValue
+	if randNotNullValue < 0.0 {
+		randNotNullValue *= -1.
+	}
+
+	randNegativeValue := randNotNullValue
+	if randNegativeValue >= 0.0 {
+		randNegativeValue *= -1.
+	}
+
+	testData := []struct {
+		caseName string
+		argValue parsedData.ArgValue
+
+		expectedRes float64
+		expectedErr bool
+	}{
+		{
+			caseName:    "empty_string_to_float64",
+			argValue:    "",
+			expectedErr: true,
+		},
+		{
+			caseName:    "float64_null",
+			argValue:    "0",
+			expectedRes: 0,
+		},
+		{
+			caseName:    "valid_positive_float64",
+			argValue:    parsedData.ArgValue(fmt.Sprintf("%v", randPositiveValue)),
+			expectedRes: randPositiveValue,
+		},
+		{
+			caseName:    "valid_negative_float64",
+			argValue:    parsedData.ArgValue(fmt.Sprintf("%v", randNegativeValue)),
+			expectedRes: randNegativeValue,
+		},
+	}
+
+	for _, td := range testData {
+		t.Run(td.caseName, func(t *testing.T) {
+			res, err := td.argValue.ToFloat64()
+			if td.expectedErr {
+				require.NotNil(t, err)
+				require.Equal(t, td.expectedRes, res)
+
+				return
+			}
+
+			require.Nil(t, err)
+			require.Equal(t, td.expectedRes, res)
+		})
+	}
+}

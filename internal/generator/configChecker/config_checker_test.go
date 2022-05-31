4eb413c8d2c48c4f6389c8db2f774673b94a932f
terryhay <terryhay@yandex.ru>
2022-05-31 22:31:31 +0300

    configChecker: 100.0% test coverage

--- internal/generator/configChecker/config_checker_test.go
+++ internal/generator/configChecker/config_checker_test.go
@@ -1,6 +1,7 @@
 package configChecker
 
 import (
+	"github.com/brianvoe/gofakeit"
 	"github.com/terryhay/argtools/internal/generator/configYaml"
 	"github.com/terryhay/argtools/pkg/argtoolsError"
 	"testing"
@@ -8,8 +9,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestConfigCheckerCorrectResponse(t *testing.T) {
+	t.Parallel()
+
+	require.Nil(t, Check(nil, nil, nil))
+}
+
 func TestConfigCheckerErrors(t *testing.T) {
 	t.Parallel()
+
+	randFlag := configYaml.Flag(gofakeit.Name())
 
 	testData := []struct {
 		caseName               string
@@ -21,10 +30,10 @@ func TestConfigCheckerErrors(t *testing.T) {
 		{
 			caseName: "duplicate_flag_in_required_list",
 			commandDescriptionMap: map[configYaml.Command]*configYaml.CommandDescription{
-				"command": {
+				configYaml.Command(gofakeit.Name()): {
 					RequiredFlags: []configYaml.Flag{
-						"flag",
-						"flag",
+						randFlag,
+						randFlag,
 					},
 				},
 			},
@@ -33,10 +42,10 @@ func TestConfigCheckerErrors(t *testing.T) {
 		{
 			caseName: "duplicate_flag_in_optional_list",
 			commandDescriptionMap: map[configYaml.Command]*configYaml.CommandDescription{
-				"command": {
+				configYaml.Command(gofakeit.Name()): {
 					OptionalFlags: []configYaml.Flag{
-						"flag",
-						"flag",
+						randFlag,
+						randFlag,
 					},
 				},
 			},
@@ -45,12 +54,12 @@ func TestConfigCheckerErrors(t *testing.T) {
 		{
 			caseName: "duplicate_flag_in_required_and_optional_lists",
 			commandDescriptionMap: map[configYaml.Command]*configYaml.CommandDescription{
-				"command": {
+				configYaml.Command(gofakeit.Name()): {
 					RequiredFlags: []configYaml.Flag{
-						"flag",
+						randFlag,
 					},
 					OptionalFlags: []configYaml.Flag{
-						"flag",
+						randFlag,
 					},
 				},
 			},
@@ -60,11 +69,11 @@ func TestConfigCheckerErrors(t *testing.T) {
 		{
 			caseName: "unused_flag",
 			commandDescriptionMap: map[configYaml.Command]*configYaml.CommandDescription{
-				"command": {},
+				configYaml.Command(gofakeit.Name()): {},
 			},
 			flagDescriptionMap: map[configYaml.Flag]*configYaml.FlagDescription{
-				"flag": {
-					Flag: "flag",
+				randFlag: {
+					Flag: randFlag,
 				},
 			},
 			expectedErrorCode: argtoolsError.CodeConfigFlagIsNotUsedInCommands,
@@ -73,9 +82,9 @@ func TestConfigCheckerErrors(t *testing.T) {
 		{
 			caseName: "undefined_required_flag",
 			commandDescriptionMap: map[configYaml.Command]*configYaml.CommandDescription{
-				"command": {
+				configYaml.Command(gofakeit.Name()): {
 					RequiredFlags: []configYaml.Flag{
-						"flag",
+						randFlag,
 					},
 				},
 			},
@@ -84,13 +93,49 @@ func TestConfigCheckerErrors(t *testing.T) {
 		{
 			caseName: "undefined_optional_flag",
 			commandDescriptionMap: map[configYaml.Command]*configYaml.CommandDescription{
-				"command": {
+				configYaml.Command(gofakeit.Name()): {
 					OptionalFlags: []configYaml.Flag{
-						"flag",
+						randFlag,
 					},
 				},
 			},
 			expectedErrorCode: argtoolsError.CodeConfigUndefinedFlag,
+		},
+
+		{
+			caseName: "null_command_description_with_duplicate_required_flags",
+			nullCommandDescription: &configYaml.NullCommandDescription{
+				RequiredFlags: []configYaml.Flag{
+					randFlag,
+					randFlag,
+				},
+			},
+
+			expectedErrorCode: argtoolsError.CodeConfigContainsDuplicateFlags,
+		},
+		{
+			caseName: "null_command_description_with_duplicate_optional_flags",
+			nullCommandDescription: &configYaml.NullCommandDescription{
+				OptionalFlags: []configYaml.Flag{
+					randFlag,
+					randFlag,
+				},
+			},
+
+			expectedErrorCode: argtoolsError.CodeConfigContainsDuplicateFlags,
+		},
+		{
+			caseName: "null_command_description_with_duplicate_required_and_optional_flags",
+			nullCommandDescription: &configYaml.NullCommandDescription{
+				RequiredFlags: []configYaml.Flag{
+					randFlag,
+				},
+				OptionalFlags: []configYaml.Flag{
+					randFlag,
+				},
+			},
+
+			expectedErrorCode: argtoolsError.CodeConfigContainsDuplicateFlags,
 		},
 	}
 
